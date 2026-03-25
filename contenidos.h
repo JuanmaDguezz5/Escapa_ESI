0c0b95aa8e539d3576e4b74c0e1f74de96653c08
JuanmaDguezz5 <juanmadopa@gmail.com>
2026-03-25 16:44:06 +0100

    Expand contents.h with new data structures

--- contenidos.h
+++ contenidos.h
@@ -4,6 +4,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    int id_jugador;
+    int id_sala;
+    int id_obj;     // Objeto: Id_obj, Localizacion)
+    int id_conexion; // Conexion: Id_conexion, Estado)
+} partida;
+
+typedef struct {
+    int id_jugador[3]; 
+    char Nomb_jugador[21]; // Nombre completo
+    char jugador[11];       // Nombre de usuario
+    char contrasenia[9];
+    int id_obj;     // ID del objeto que posee el jugador, 0 si no tiene ninguno, tantos codigos como objetos posea    
+} jugadores;
+
+typedef struct {
+    char id_puzle[4];      
+    char nombre[16];       
+    int id_sala;          
+    char tipo[10];         // Código o Palabra 
+    char descripcion[151]; 
+    char solucion[51];     
+    int resuelto;          // 0 para Pendiente, 1 para Resuelto 
+} puzle;
+
+typedef struct {
+    char id_objeto[5];       
+    char nombre[16];       
+    char descripcion[151];
+    char lugar[11];         //Id_sala o Inventario 
+} objeto;
+
+typedef struct {
+    char id_conexion[4];       
+    char id_origen[3];       
+    char id_destino[3];
+    char Estado[11];    // Activa o Bloqueada
+    int condicion; // 0 si no aplica, Id_obj o Id_puzle
+} conexiones;
+
+typedef struct {
+    char id_sala[4];       
+    char nombre[16];       
+    char Tipo[11]; //Inicial, normal o salida
+    char descripcion[151];
+} salas;
+
 // Muestra la descripción de la sala y verifica si es la SALIDA 
 void describirSala(salas *salaActual); 
 
